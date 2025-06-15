2408eaec7445c8993c549aa9bb0883c3888f635e
Weerawat-M <weerawat1604@go.byuh.edu>
2025-06-14 14:37:55 -1000

    15

---
+++ Activity-15/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
+
